de45c3f60549ce746f8daf2e0ce14486fb2d4fa6
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-03-10 16:03:36 +0530

    adding the auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,38 @@
 const jwt= require('jsonwebtoken');
+const httpStatusCode = require('../constant/httpStatusCode');
+require('dotenv').config();
+const {JWT_SECRET}=process.env;
+
+const GetToken=async (user)=>{
+    const token= await jwt.sign({user},JWT_SECRET,{expiresIn:'1d'});
+    return token;
+}
+
+const VerifyToken=(req,res,next)=>{
+    const token= req.headers.authorization;
+    if(!token){
+        return res.status(httpStatusCode.UNAUTHORIZED).json({
+            success:false,
+            message:"Unauthorized: Token not provided"
+        })
+    }
+
+    try{
+
+        const decoded= jwt.verify(token,JWT_SECRET);
+        req.user=decoded.user;
+        console.log(req.user);
+        next();
+    }catch(error){
+        return res.status(httpStatusCode.UNAUTHORIZED).json({
+            success: false,
+            message:"Unauthorized: Invalid token"
+        })
+    }
+}
 
 
+module.exports={
+    GetToken,
+    VerifyToken
+}
